f8d94d2d81d7872af31f316e2ce7099993d4bfdd
OGRaul <rauloteroguzman@gmail.com>
2022-11-07 18:46:56 +0100

    init

--- Buscaminas/app/src/main/java/pmdm/example/buscaminas/Juego.java
+++ Buscaminas/app/src/main/java/pmdm/example/buscaminas/Juego.java
@@ -29,12 +29,6 @@ public class Juego extends MainActivity{
     private boolean derrota = false;
     private boolean victoria = false;
 
-    final static float STEP = 200;
-    float mRatio = 1.0f;
-    int mBaseDist;
-    float mBaseRatio;
-    float fontsize = 13;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -291,28 +285,5 @@ public class Juego extends MainActivity{
                 break;
         }
         return tamano;
-    }
-
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getPointerCount() == 2) {
-            int action = event.getAction();
-            int pureaction = action & MotionEvent.ACTION_MASK;
-            if (pureaction == MotionEvent.ACTION_POINTER_DOWN) {
-                mBaseDist = getDistance(event);
-                mBaseRatio = mRatio;
-            } else {
-                float delta = (getDistance(event) - mBaseDist) / STEP;
-                float multi = (float) Math.pow(2, delta);
-                mRatio = Math.min(1024.0f, Math.max(0.1f, mBaseRatio * multi));
-                gridLayout.setScaleX(mRatio + 13);
-            }
-        }
-        return true;
-    }
-
-    int getDistance(MotionEvent event) {
-        int dx = (int) (event.getX(0) - event.getX(1));
-        int dy = (int) (event.getY(0) - event.getY(1));
-        return (int) (Math.sqrt(dx * dx + dy * dy));
     }
 }
